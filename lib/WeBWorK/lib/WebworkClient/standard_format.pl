2ea0fab9b0652a2744da3ccb680873c8de6de0f0
doombot-exe <68438287+doombot-exe@users.noreply.github.com>
2020-06-19 17:11:50 +0100

    update form support for applets

--- lib/WeBWorK/lib/WebworkClient/standard_format.pl
+++ lib/WeBWorK/lib/WebworkClient/standard_format.pl
@@ -36,7 +36,7 @@ $problemHeadText
 <h2> WeBWorK using host: $SITE_URL, course: $courseID format: standard</h2>
 		    $answerTemplate
 		    $color_input_blanks_script
-	<form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post">
+	<form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post" onsubmit"submitAction()">
 <div id="problem_body" class="problem-content" $PROBLEM_LANG_AND_DIR>
 			$problemText
 </div>
