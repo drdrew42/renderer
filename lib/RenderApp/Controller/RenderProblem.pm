2af0b92962c5af3539b4982f3f5a5110cad121af
doombot-exe <68438287+doombot-exe@users.noreply.github.com>
2020-08-25 12:41:39 -0400

    drop the encoded_source - because WHY

--- lib/RenderApp/Controller/RenderProblem.pm
+++ lib/RenderApp/Controller/RenderProblem.pm
@@ -214,7 +214,7 @@ sub process_problem {
 	#my $encoded_source = encode_base64($source); # create encoding of source_file;
 	my $formatter = RenderApp::Controller::FormatRenderedProblem->new(
 		return_object    => $return_object,
-		encoded_source   => encode_base64($source),
+		encoded_source   => '', #encode_base64($source),
 		sourceFilePath   => $file_path,
 		url              => $inputs_ref->{base_url} || $inputs_ref->{baseURL},   # use default hosted2
 		form_action_url  => $inputs_ref->{form_action_url} || $inputs_ref->{formURL},
