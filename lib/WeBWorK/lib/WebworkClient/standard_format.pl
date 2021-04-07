744c625885b8f334dedf4a01347442dd1a2b661f
K. Andrew Parker <kparker@citytech.cuny.edu>
2021-04-07 11:43:37 -0400

    update format templates for pg 2.16

--- lib/WeBWorK/lib/WebworkClient/standard_format.pl
+++ lib/WeBWorK/lib/WebworkClient/standard_format.pl
@@ -6,30 +6,40 @@ $standard_format = <<'ENDPROBLEMTEMPLATE';
 <link rel="shortcut icon" href="$webwork_htdocs_url/images/favicon.ico"/>
 
 <!-- CSS Loads -->
-<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/css/jquery-ui.css"/>
+<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/node_modules/jquery-ui-dist/jquery-ui.min.css"/>
+<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/vendor/bootstrap/css/bootstrap.min.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/vendor/bootstrap/css/bootstrap-responsive.min.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/css/bootstrap.sub.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/themes/math4/math4.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/css/knowlstyle.css"/>
+
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/apps/MathQuill/mathquill.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/apps/MathQuill/mqeditor.css"/>
+<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/apps/ImageView/imageview.css"/>
+
+$extra_css_files
 
 <!-- JS Loads -->
 <script>function submitAction() {}</script>
-<script type="text/javascript" src="$webwork_htdocs_url/js/jquery.min.js"></script>
-<script type="text/javascript" src="$webwork_htdocs_url/js/jquery-ui.min.js"></script>
-<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/latest.js?config=TeX-MML-AM_CHTML"></script>
-<script type="text/javascript" src="$webwork_htdocs_url/js/apps/AddOnLoad/addOnLoadEvent.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
+<script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathJaxConfig/mathjax-config.js" defer></script>
+<script type="text/javascript" src="$webwork_htdocs_url/mathjax/es5/tex-chtml.js" id="MathJax-script" defer></script>
+<script type="text/javascript" src="$webwork_htdocs_url/node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="$webwork_htdocs_url/node_modules/jquery-ui-dist/jquery-ui.min.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/legacy/java_init.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/InputColor/color.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/Base64/Base64.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/legacy/vendor/knowl.js"></script>
+<script type="text/javascript" src="$webwork_htdocs_url/js/apps/ImageView/imageview.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/themes/math4/math4.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/vendor/iframe-resizer/js/iframeResizer.contentWindow.min.js"></script>
+
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathQuill/mathquill.min.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathQuill/mqeditor.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/submithelper.js"></script>
+
+$extra_js_files
 
 $problemHeadText
 $problemPostHeaderText
