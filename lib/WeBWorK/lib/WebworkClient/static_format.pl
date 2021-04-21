285905d73cd07b98d9ac950e1c1dfff5d5a84a0d
Henry Agnew <henryd.agnew@gmail.com>
2021-04-20 19:51:42 -0700

    Replaced tags with CDN when possible

--- lib/WeBWorK/lib/WebworkClient/static_format.pl
+++ lib/WeBWorK/lib/WebworkClient/static_format.pl
@@ -7,8 +7,8 @@ $static_format = <<'ENDPROBLEMTEMPLATE';
 <link rel="shortcut icon" href="$webwork_htdocs_url/images/favicon.ico"/>
 
 <!-- CSS Loads -->
-<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/node_modules/jquery-ui-dist/jquery-ui.min.css"/>
-<link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/vendor/bootstrap/css/bootstrap.min.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/js/vendor/bootstrap/css/bootstrap-responsive.min.css"/>
 <link rel="stylesheet" type="text/css" href="$webwork_htdocs_url/css/bootstrap.sub.css"/>
@@ -25,19 +25,19 @@ $extra_css_files
 <script>function submitAction() {}</script>
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathJaxConfig/mathjax-config.js" defer></script>
-<script type="text/javascript" src="$webwork_htdocs_url/mathjax/es5/tex-chtml.js" id="MathJax-script" defer></script>
-<script type="text/javascript" src="$webwork_htdocs_url/node_modules/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="$webwork_htdocs_url/node_modules/jquery-ui-dist/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.1.2/es5/tex-chtml.min.js" defer integrity="sha512-OEN4O//oR+jeez1OLySjg7HPftdoSaKHiWukJdbFJOfi2b7W0r0ppziSgVRVNaG37qS1f9SmttcutYgoJ6rwNQ==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/legacy/java_init.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/InputColor/color.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/Base64/Base64.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/legacy/vendor/knowl.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/ImageView/imageview.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/themes/math4/math4.js"></script>
-<script type="text/javascript" src="$webwork_htdocs_url/js/vendor/iframe-resizer/js/iframeResizer.contentWindow.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.1/iframeResizer.contentWindow.min.js" integrity="sha512-qw2bX9KUhi7HLuUloyRsvxRlWJvj0u0JWVegc5tf7qsw47T0pwXZIk1Kyc0utTH3NlrpHtLa4HYTVUyHBr9Ufg==" crossorigin="anonymous"></script>
 
 <script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathQuill/mathquill.min.js"></script>
-<script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathQuill/mqstatic.js"></script>
+<script type="text/javascript" src="$webwork_htdocs_url/js/apps/MathQuill/mqeditor.js"></script>
 <script type="text/javascript" src="$webwork_htdocs_url/js/submithelper.js"></script>
 
 $extra_js_files
