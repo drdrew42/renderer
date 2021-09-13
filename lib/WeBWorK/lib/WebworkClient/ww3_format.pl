1a5d659b2b6a3740c1f65a7ba14bf36788d909e1
K. Andrew Parker <kparker@citytech.cuny.edu>
2021-09-13 14:57:45 -0700

    strip everything down to the form tags

--- lib/WeBWorK/lib/WebworkClient/ww3_format.pl
+++ lib/WeBWorK/lib/WebworkClient/ww3_format.pl
@@ -1,36 +1,28 @@
 $ww3_format = <<'ENDPROBLEMTEMPLATE';
 <!DOCTYPE html>
-<html $COURSE_LANG_AND_DIR>
-  <div class="container-fluid">
-    <div class="row-fluid">
-      <div class="span12 problem">
-		    $answerTemplate
-        $color_input_blanks_script
-        <form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post" onsubmit="submitAction()">
-          <div id="problem_body" class="problem-content" $PROBLEM_LANG_AND_DIR>
-            $problemText
-          </div>
-          $scoreSummary
-          $LTIGradeMessage
-
-          <input type="hidden" name="answersSubmitted" value="1">
-          <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
-          <input type="hidden" name="problemSourceURL" value = "$problemSourceURL">
-          <input type="hidden" name="problemSource" value="$encoded_source">
-          <input type="hidden" name="problemSeed" value = "$problemSeed">
-          <input type="hidden" name="language" value="$formLanguage">
-          <input type="hidden" name="showSummary" value="$showSummary">
-          <p>
-
-            <input type="submit" name="previewAnswers"  value="$STRING_Preview" />
-            <input type="submit" name="submitAnswers" value="$STRING_Submit"/>
-            <input type="submit" name="showCorrectAnswers" value="$STRING_ShowCorrect"/>
-          </p>
-        </form>
-      </div>
-    </div>
+$answerTemplate
+$color_input_blanks_script
+<form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post" onsubmit="submitAction()">
+  <div id="problem_body" class="problem-content" $PROBLEM_LANG_AND_DIR>
+    $problemText
   </div>
-</html>
+  $scoreSummary
+  $LTIGradeMessage
+
+  <input type="hidden" name="answersSubmitted" value="1">
+  <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
+  <input type="hidden" name="problemSourceURL" value = "$problemSourceURL">
+  <input type="hidden" name="problemSource" value="$encoded_source">
+  <input type="hidden" name="problemSeed" value = "$problemSeed">
+  <input type="hidden" name="language" value="$formLanguage">
+  <input type="hidden" name="showSummary" value="$showSummary">
+  <p>
+
+    <input type="submit" name="previewAnswers"  value="$STRING_Preview" />
+    <input type="submit" name="submitAnswers" value="$STRING_Submit"/>
+    <input type="submit" name="showCorrectAnswers" value="$STRING_ShowCorrect"/>
+  </p>
+</form>
 
 ENDPROBLEMTEMPLATE
 
