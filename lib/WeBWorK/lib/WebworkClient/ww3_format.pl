9a9f74484fd2558ba83d5238ab722077bc24a428
Glenn Rice <grice1@missouriwestern.edu>
2021-09-21 09:33:19 -0500

    Make the ww3 format output json data.  For now it only separates the results table from the rest.  More will be needed.

--- lib/WeBWorK/lib/WebworkClient/ww3_format.pl
+++ lib/WeBWorK/lib/WebworkClient/ww3_format.pl
@@ -1,30 +1,28 @@
-$ww3_format = <<'ENDPROBLEMTEMPLATE';
-<!DOCTYPE html>
+{
+	answerTemplate => '$answerTemplate',
+
+	problemText => <<'ENDPROBLEMTEMPLATE'
 $problemHeadText
-$answerTemplate
 $color_input_blanks_script
 <form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post">
-  <div id="problem_body" class="problem-content" $PROBLEM_LANG_AND_DIR>
-    $problemText
-  </div>
-  $scoreSummary
-  $LTIGradeMessage
+	<div id="problem_body" class="problem-content" $PROBLEM_LANG_AND_DIR>
+		$problemText
+	</div>
+	$scoreSummary
+	$LTIGradeMessage
 
-  <input type="hidden" name="answersSubmitted" value="1">
-  <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
-  <input type="hidden" name="problemSourceURL" value = "$problemSourceURL">
-  <input type="hidden" name="problemSource" value="$encoded_source">
-  <input type="hidden" name="problemSeed" value = "$problemSeed">
-  <input type="hidden" name="language" value="$formLanguage">
-  <input type="hidden" name="showSummary" value="$showSummary">
-  <p>
-
-    <input type="submit" name="previewAnswers"  value="$STRING_Preview" />
-    <input type="submit" name="submitAnswers" value="$STRING_Submit"/>
-    <input type="submit" name="showCorrectAnswers" value="$STRING_ShowCorrect"/>
-  </p>
+	<input type="hidden" name="answersSubmitted" value="1">
+	<input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
+	<input type="hidden" name="problemSourceURL" value = "$problemSourceURL">
+	<input type="hidden" name="problemSource" value="$encoded_source">
+	<input type="hidden" name="problemSeed" value = "$problemSeed">
+	<input type="hidden" name="language" value="$formLanguage">
+	<input type="hidden" name="showSummary" value="$showSummary">
+	<p>
+	<input type="submit" name="previewAnswers"  value="$STRING_Preview" />
+	<input type="submit" name="submitAnswers" value="$STRING_Submit"/>
+	<input type="submit" name="showCorrectAnswers" value="$STRING_ShowCorrect"/>
+	</p>
 </form>
-
 ENDPROBLEMTEMPLATE
-
-$ww3_format;
+};
